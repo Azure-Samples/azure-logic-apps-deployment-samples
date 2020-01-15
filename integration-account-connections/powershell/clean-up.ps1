ba1dc89f0b63c1a82a6448053dd2bae41bdd690a
cdmdotnet <matt@cdmdotnet.com>
2020-01-16 12:16:08 +1300

    Fixing issue #9
    Moved the closing bracket to the correct place.

--- integration-account-connections/powershell/clean-up.ps1
+++ integration-account-connections/powershell/clean-up.ps1
@@ -15,7 +15,7 @@ param(
 $resourceGroups = Get-AzResourceGroup;
 
 foreach ($resourceGroup in $resourceGroups) {
-    if ($resourceGroup.ResourceGroupName.StartsWith($groupId), 'CurrentCultureIgnoreCase') {
+    if ($resourceGroup.ResourceGroupName.StartsWith($groupId, 'CurrentCultureIgnoreCase')) {
         Remove-AzResourceGroup -Name $resourceGroup.ResourceGroupName -Force -AsJob
         Write-Host "Deleted resource group $($resourceGroup.ResourceGroupName)"
     }
